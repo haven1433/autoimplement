bd1bcf9a5f5f8973449f3fb90dc6c6f2c62b1e97
Benjamin Popp <haven1433@gmail.com>
2018-10-06 16:02:12 -0500

    adding some documentation to System.Delegation and creating .xml for it. This is meant to be used updating version numbers 1.0.0 -> 1.0.1 using AutoImplement to generate the Event-using class that we want for functional testing, to verify that the event works as expected.

--- AutoImplementTests/FunctionalTests.cs
+++ AutoImplementTests/FunctionalTests.cs
@@ -1,14 +1,15 @@
-﻿using Xunit;
+﻿using System.ComponentModel;
+using Xunit;
 
 namespace AutoImplementTests {
    public class FunctionalTests {
       [Fact]
       public void CustomEventStubClassActuallyWorksAsExpect() {
-         void HelperMethod(object sender, int args) { }
-         var implementation = new StubHasCustomEvent();
-         implementation.CustomEvent += HelperMethod;
-         implementation.CustomEvent -= HelperMethod;
-         Assert.Empty(implementation.CustomEvent.handlers);
+         void HelperMethod(object sender, PropertyChangedEventArgs args) { }
+         var implementation = new StubNotifyPropertyChanged();
+         implementation.PropertyChanged += HelperMethod;
+         implementation.PropertyChanged -= HelperMethod;
+         Assert.Empty(implementation.PropertyChanged.handlers);
       }
    }
 }
--- System.Delegation/PropertyImplementation.cs
+++ System.Delegation/PropertyImplementation.cs
@@ -36,8 +36,26 @@ namespace System.Delegation
          get = () => value;
       }
 
+      /// <summary>
+      /// Assigning a PropertyImplementation to a normal value will use any custom get delegate that you've setup.
+      /// If you haven't setup a custom get delegate, then the PropertyImplementation's value is used.
+      /// </summary>
+      /// <example>
+      /// var property = new PropertyImplementation&lt;int&gt;();
+      /// property.get = () => 4;
+      /// property.value = 7;
+      /// 
+      /// int x = property; // x is now 4.
+      /// </example>
       public static implicit operator T(PropertyImplementation<T> cast) => cast.get();
 
+      /// <summary>
+      /// Assigning a normal value to a PropertyImplementation will remove any get/set delegates you've added to the property implementation.
+      /// </summary>
+      /// <example>
+      /// var property = new PropertyImplementation&lt;int&gt;();
+      /// property = 7;
+      /// </example>
       public static implicit operator PropertyImplementation<T>(T cast) => new PropertyImplementation<T>(cast);
    }
 }
