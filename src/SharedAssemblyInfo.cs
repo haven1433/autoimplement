06f15f8646b16d078250c56e45b4c75b148dc94f
Benjamin Popp <haven1433@gmail.com>
2018-11-18 22:19:14 -0600

    bumping version. After all the changes for stubs for classes, this is a new feature release, not a bugfix release.

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
